81fc20ba7df1df039e54349e7865bcd28cb39baa
Raghu <rdgowda79@gmail.com>
2022-12-07 11:05:34 +0530

    Simple MongoRepository demo added.

---
+++ src/main/java/com/employee/demo/Employee.java
@@ -0,0 +1,88 @@
+package com.employee.demo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.annotation.Generated;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Document//("employees")
+public class Employee {
+	@Id
+	private String id;
+	//@NotBlank(message = "Employee name can't be blank.")
+	//@Size(min=3, max=30, message = "Name must be at least 3 and max 30 chars")
+	//@NotNull(message = "Employee name can't be null.")
+	//@Pattern(regexp="[A-Za-z]{3,30}", message= "Name must be alphabets having min 3 to max 30 chars.")
+	private String name;
+	//@Min(value = 0,message = "Salary must be greater than 0.")	
+	private Double salary;
+	// employee has depencdency on Address
+	
+	
+	//private Address address;
+	
+	
+	
+	//private Department department;
+	
+	public Employee() {
+		super();
+	}
+	public Employee(String id, String name, Double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	
+//	public Employee(Integer id, String name, Double salary, Address address, Department department) {
+//		super();
+//		this.id = id;
+//		this.name = name;
+//		this.salary = salary;
+//		this.address = address;
+//		this.department = department;
+//	}
+	
+//	public Department getDepartment() {
+//		return department;
+//	}
+//	public void setDepartment(Department department) {
+//		this.department = department;
+//	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+//	public Address getAddress() {
+//		return address;
+//	}
+//	public void setAddress(Address address) {
+//		this.address = address;
+//	}
+	
+	
+}
---
+++ src/main/java/com/employee/demo/EmployeeController.java
@@ -0,0 +1,41 @@
+package com.employee.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepo;
+	
+	@GetMapping("/")
+	public String greet() {
+		return "Mongo Repository Demo";
+	}
+	@PostMapping("employee")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		return this.employeeRepo.save(employee);
+	}
+	@GetMapping("employee/{id}")
+	public Employee getEmployeeById(@PathVariable("id") String id) {
+		return this.employeeRepo.findById(id).get();
+	}
+	@PutMapping("employee")
+	public Employee updateEmployee(@RequestBody Employee employee) {
+		return this.employeeRepo.save(employee);
+	}
+	@DeleteMapping("employee/{id}")
+	public Employee deleteEmployeeById(@PathVariable("id") String id) {
+		Employee emp =this.employeeRepo.findById(id).get();
+		
+		this.employeeRepo.deleteById(id);
+		return emp;
+	}
+}
